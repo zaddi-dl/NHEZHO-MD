551a0c9616a54c78c2a543fba2fe7bac6eaa64cd
caseyweb <caseywebsthedonn25@gmail.com>
2024-10-28 17:44:48 +0300

    CASEYRHODES🌟

--- commandes/menu.js
+++ commandes/menu.js
@@ -51,7 +51,7 @@ const date = moment().format('DD/MM/YYYY');
         menuMsg += `*╭────❂* *${cat}* *❂*`;
         for (const cmd of coms[cat]) {
             menuMsg += `  
-*┊♣️* ${cmd}`;
+*┊💎* ${cmd}`;
         }
         menuMsg += `
 *╰═════════════❂* \n`
