39b648f69ff42895149de66e7cdc57af2fafcc41
caseyweb <caseywebsthedonn25@gmail.com>
2024-11-28 23:59:45 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 zokou({ nomCom: "test", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '*⭕𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂 𝙼𝙳 𝙸𝚂 𝙾𝙽𝙻𝙸𝙽𝙴 ⭕* 🙏 \n\n ' + "𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂 𝙼𝙳 𝙸𝚂 𝙲𝙰𝙽𝚃 𝚂𝙻𝙴𝙴𝙿⏰⭕";
-    let d = '                                                                           𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂 𝚃𝙴𝙲𝙷🍀';
+    let z = '*⭕𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂 𝙼𝙳 𝙸𝚂 𝙾𝙽𝙻𝙸𝙽𝙴 ⭕* 🙏 \n\n ' + "𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂 𝙼𝙳 𝙲𝙰𝙽𝚃 𝚂𝙻𝙴𝙴𝙿⏰⭕";
+    let d = '                                                                          > 𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 𝙲𝙰𝚂𝙴𝚈𝚁𝙷𝙾𝙳𝙴𝚂 𝚃𝙴𝙲𝙷🍀';
     let varmess = z + d;
     var mp4 = 'https://files.catbox.moe/nk1ppr.mp4';
     await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
