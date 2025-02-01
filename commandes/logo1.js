58d32a73a326e2aefda1216ba7291bd91f947e42
caseyweb <caseywebsthedonn25@gmail.com>
2025-02-01 22:30:33 +0300

    𝐜𝐚𝐬𝐞𝐲𝐫𝐡𝐨𝐝𝐞𝐬🫅

--- commandes/logo1.js
+++ commandes/logo1.js
@@ -258,7 +258,7 @@ zokou({ nomCom: "logo", categorie: "Search", reaction: "✋" }, async (dest, zk,
         if (logoUrl) {
           await zk.sendMessage(dest, {
             image: { url: logoUrl },
-            caption: `*Downloaded by Alpha Md*`,
+            caption: `*POWERED BY CASEYRHODES-XMD*`,
           }, { quoted: ms });
         }
       }
